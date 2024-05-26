99a705c85b5f33f71602c46706e7548afc193f47
TimWong <timwong.tw93@gmail.com>
2024-05-26 20:29:48 +1000

    made inquirer prompt loop until quit is selected

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
 const inquirer = require(`inquirer`);
+const { Pool } = require('pg');
 
-inquirer
-  .prompt ([
-    {
-      type: `list`,
-      message: `What would you like to do?`,
-      choices: [`View All Employees`, `Add Employee`, `Update Employee Role`, `View all Roles`, `Add Role`, `View All Departments`, `Add Department`, `Quit`],
-      name: `userChoice`,
-    }
-  ])
-  .then((response) => {
-    console.log(`This is your choice: ${response.userChoice}`);
-  })
-  .catch((err) => {
-    console.log(err);
-    console.log('Oops. Something went wrong.');
-  });
+// Connect to database
+const pool = new Pool(
+  {
+    // Enter PostgreSQL username
+    user: 'postgres',
+    // Enter PostgreSQL password
+    password: 'postgres',
+    host: 'localhost',
+    database: 'employees_db'
+  },
+console.log('Connected to the employees_db database!')
+)
+
+pool.connect();
+
+const main = async () => {
+  let choice = ``;
+  while(choice !== `Quit`) {
+    await inquirer
+    .prompt ([
+      {
+        type: `list`,
+        message: `What would you like to do?`,
+        choices: [`View All Employees`, `Add Employee`, `Update Employee Role`, `View All Roles`, `Add Role`, `View All Departments`, `Add Department`, `Quit`],
+        name: `userChoice`,
+      }
+    ])
+    .then((response) => {
+      choice = response.userChoice;
+      switch(response.userChoice) {
+        case `View All Employees`:
+          pool.query('SELECT * FROM employee', function (err, {rows}) {
+            // console.log(rows);
+          });
+          break;
+        case `View All Roles`:
+          pool.query('select role.id as "ID", role.title as "Title", department.name as "Department", role.salary as "Salary" from role join department on role.department_id  = department.id;', function (err, {rows}) {
+            console.log(rows);
+          });
+          break;
+        case `View All Departments`:
+          pool.query('SELECT * FROM employee', function (err, {rows}) {
+            // console.log(rows);
+          });
+          break;
+        case `Quit`:
+          break;
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log('Oops. Something went wrong.');
+    });
+  }
+}
+
+main();
+
+
