87f92d17cc3dc385823427e1aa785354c39214fc
TimWong <timwong.tw93@gmail.com>
2024-05-29 00:45:15 +1000

    updated query.sql with queries used in project

--- db/query.sql
+++ db/query.sql
@@ -1,16 +1,19 @@
--- select
---   department.id as "ID",
---   department.name as "Department"
--- from department;
+-- see all departments
+select
+  department.id as "ID",
+  department.name as "Department"
+from department;
 
--- select 
---   role.id as "ID", 
---   role.title as "Title", 
---   department.name as "Department",
---   role.salary as "Salary"
--- from role
--- join department on role.department_id  = department.id;
+-- see all roles, their salaries and which department it belongs to
+select 
+  role.id as "ID", 
+  role.title as "Title", 
+  department.name as "Department",
+  role.salary as "Salary"
+from role
+join department on role.department_id  = department.id;
 
+-- see all employees, their roles, which department they belong to, their salaries and who their manager is
 select
   e1.id as "ID",
   e1.first_name as "First Name",
@@ -24,16 +27,46 @@ join role on e1.role_id = role.id
 join department on role.department_id = department.id
 left join employee e2 on e1.manager_id = e2.id;
 
+-- select a manager and e2.id and see which employees work under said manager
 select 
-  concat(e1.first_name, ' ', e1.last_name) as "Name",
-  concat(e2.first_name, ' ', e2.last_name) as "Manager"
+  concat(e2.first_name, ' ', e2.last_name) as "Manager",
+  concat(e1.first_name, ' ', e1.last_name) as "Employee"
   from employee e1
-  left join employee e2 on e1.manager_id = e2.id;
+  left join employee e2 on e1.manager_id = e2.id
+  where e2.id = 1;
 
+-- select all managers
 select
   e1.manager_id as "value",
   concat(e2.first_name, ' ', e2.last_name) as "name"
   from employee e1
   inner join employee e2 on e1.manager_id = e2.id;
+
+-- select departments and the roles within the department
+select
+  department.name as "Department",
+  role.title as "Role"
+  from department
+  join role on department.id = role.department_id;
+
+-- select departments, the roles within the department and the employees working in corresponding department/roles
+select
+  department.name as "Department",
+  role.title as "Role",
+  concat(employee.first_name, ' ', employee.last_name) as "Employee"
+  from employee
+  join role on employee.role_id = role.id
+  join department on role.department_id = department.id;
+
+-- select department and their total salaries
+select
+  department.name as "Department",
+  sum(role.salary) as "Salary"
+  from role
+  join department on role.department_id = department.id
+  where department.id = 2
+  group by department.name;
+
+
 
 
